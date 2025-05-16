7fbae31a323523a2f5e60d521c4cc8185af2a249
yusuketamai <k024g1034@g.neec.ac.jp>
2025-05-16 14:41:30 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,58 +1,60 @@
-﻿#include<stdio.h>
-#include<Windows.h>
-#include <time.h>
-#include <functional>
+﻿#include <iostream>
+using namespace std;
 
+class Enemy {
+public:
+    void Update();
 
-//コールバック関数
-void DispResult(int* s, int* kye) {
-	int dice = rand() % 2;
+    void Approach(); // 接近
+    void Attack(); // 攻撃
+    void Withdrawal(); // 離脱
 
-	if (dice == *kye) {
-		if (dice == 0)
-			printf("%dで丁(偶数)でした。当たり", dice);
-		else
-			printf("%dで半(奇数)でした。当たり", dice);
-	}
-	else {
-		if (dice == 1)
-			printf("%dで半(奇数)でした。はずれ", dice);
-		else
-			printf("%dで丁(偶数)でした。はずれ", dice);
-	}
+    // 関数ポインタテーブル
+    static void (Enemy::*pfanc[])();
 
+private:
+    int index = 0;
+};
+
+void Enemy::Approach() {
+    cout << "敵が接近！" << endl;
 }
 
-void setTimeout(std::function<void(int*, int*)>p, int second, int kye) {
-	//コールバック関数を呼び出す
-	for (int i = 0; i < second; i++) {
-		Sleep(1000);
-		printf("%d...\n", second - i);
-	}
-
-
-	p(&second, &kye);
+void Enemy::Attack() {
+    cout << "敵が攻撃！" << endl;
 }
+
+void Enemy::Withdrawal() {
+    cout << "敵が離脱" << endl;
+}
+
+void Enemy::Update() {
+
+    // 関数テーブルから関数を実行
+    (this->*pfanc[index])();
+
+    cout << "次の状態に移行 (0: はい、 他: いいえ)";
+    int input;
+    cin >> input;
+
+    if (input == 0) {
+        index = (index + 1) % 3;
+    }
+}
+
+// メンバ関数ポインタテーブル
+void (Enemy::* Enemy::pfanc[])() = {
+    &Enemy::Approach, // インデックス0
+    &Enemy::Attack,   // インデックス1
+    &Enemy::Withdrawal   // インデックス2
+};
 
 int main() {
 
-	int kye;
+    Enemy enemy;
 
-	srand(static_cast<unsigned int>(time(NULL)));
-	printf("丁(偶数)なら0、半(奇数)なら1を打つ\n");
-	scanf_s("%d", &kye);
+    while (1)enemy.Update();
 
-	if (kye == 0) {
-		puts("あなたは丁(偶数)を選びました");
-	}
-	else {
-		puts("あなたは半(奇数)を選びました");
-	}
-
-	std::function<void (int*s,int*key)>p =[](int* s, int* kye) {DispResult(s, kye); };
-
-	setTimeout(p, 3, kye);
-
-	return 0;
-};
+    return 0;
+}
 
