c5f5dd2c01c4598138e1aa4acafd4e3a5b420fbd
yusuketamai <k024g1034@g.neec.ac.jp>
2025-05-16 14:21:25 +0900

    aa

--- main.cpp
+++ main.cpp
@@ -1,23 +1,58 @@
-﻿#include <stdio.h>
+﻿#include<stdio.h>
+#include<Windows.h>
+#include <time.h>
+#include <functional>
 
-int Recursive1(int kyuuryou, int zikan, int kotei) {
-	if (kyuuryou >= kotei) {
-		printf("%d時間後に超える", zikan);
-		return zikan;
+
+//コールバック関数
+void DispResult(int* s, int* kye) {
+	int dice = rand() % 2;
+
+	if (dice == *kye) {
+		if (dice == 0)
+			printf("%dで丁(偶数)でした。当たり", dice);
+		else
+			printf("%dで半(奇数)でした。当たり", dice);
 	}
-	printf("%d　%d\n", kyuuryou, kotei);
-	kyuuryou = kyuuryou *2 - 50;
+	else {
+		if (dice == 1)
+			printf("%dで半(奇数)でした。はずれ", dice);
+		else
+			printf("%dで丁(偶数)でした。はずれ", dice);
+	}
 
-	return (Recursive1(kyuuryou, ++zikan, kotei +=1072));
+}
+
+void setTimeout(std::function<void(int*, int*)>p, int second, int kye) {
+	//コールバック関数を呼び出す
+	for (int i = 0; i < second; i++) {
+		Sleep(1000);
+		printf("%d...\n", second - i);
+	}
+
+
+	p(&second, &kye);
 }
 
 int main() {
 
-	int saiki = 100;
-	int ippan = 1072;
-	int zikan = 0;
-	int resullt1 = Recursive1(saiki, zikan, ippan);;
+	int kye;
+
+	srand(static_cast<unsigned int>(time(NULL)));
+	printf("丁(偶数)なら0、半(奇数)なら1を打つ\n");
+	scanf_s("%d", &kye);
+
+	if (kye == 0) {
+		puts("あなたは丁(偶数)を選びました");
+	}
+	else {
+		puts("あなたは半(奇数)を選びました");
+	}
+
+	std::function<void (int*s,int*key)>p =[](int* s, int* kye) {DispResult(s, kye); };
+
+	setTimeout(p, 3, kye);
 
 	return 0;
-}
+};
 
