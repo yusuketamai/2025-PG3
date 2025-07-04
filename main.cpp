0d9169de37f1cece4b1f9c147715b194a9a83bcc
yusuketamai <k024g1034@g.neec.ac.jp>
2025-07-04 15:28:19 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,25 +1,45 @@
 ﻿#include <stdio.h>
-#include "IShape.h"
-#include "Circle.h"
-#include "Rectangle.h"
+#include <list>
+#include <iostream>
+
+using namespace std;
 
 int main(void) {
-
-	IShape* shape[2];
-
-	shape[0] = new Circle;
-	shape[1] = new Rectangle;
-
-	printf("円の半径:4\n短形の底辺:2\n短形の高さ:3\n");
-
-	for (int i = 0; i < 2; i++)
-		shape[i]->Size();
+	//初めは1970年の駅名リスト
+	list <const char*> eki_list{
+		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani", "Nippori", "Tabata", "Komagome", "Sugamo", "Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Sin-Okubo", "Shinjuku", "Yoyogi", "Harajuku", "Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki", "Shinagawa", "Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
+	};
+	
 
 
-	printf("\n");
+	printf("1970年\n");
+	for (list<const char*>::iterator it_f = eki_list.begin(); it_f != eki_list.end(); it_f++) {
+		std::cout << *it_f << endl;
+	}
 
-	for (int i = 0; i < 2; i++)
-		shape[i]->Draw();
+	printf("\n2019年\n");
+	list<const char*>::iterator itr;
+	for (list<const char*>::iterator it_f = eki_list.begin(); it_f != eki_list.end(); it_f++) {
+
+		if (it_f == eki_list.insert(it_f, "Nippori")) {
+			it_f = eki_list.insert(it_f, "Nishi-Nippori");
+			std::cout << *it_f << endl;
+			++it_f;
+		}
+		std::cout << *it_f << endl;
+	}
+
+	printf("\n2022年\n");
+	for (list<const char*>::iterator it_f = eki_list.begin(); it_f != eki_list.end(); it_f++) {
+
+		if (it_f == eki_list.insert(it_f, "Shinagawa")) {
+			it_f = eki_list.insert(it_f, "Takanawa Gateway");
+			std::cout << *it_f << endl;
+			++it_f;
+		}
+		std::cout << *it_f << endl;
+	}
 
 	return 0;
 }
+
